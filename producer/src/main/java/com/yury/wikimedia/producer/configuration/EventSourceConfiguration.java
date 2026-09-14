ad124ff6d388a2124808665a9c2383faf3544762
Yury Yaroshevich <j.jaroshevich@kpedev.by>
2026-09-14 15:31:45 +0300

    some configs

--- producer/src/main/java/com/yury/wikimedia/producer/configuration/EventSourceConfiguration.java
+++ producer/src/main/java/com/yury/wikimedia/producer/configuration/EventSourceConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.URI;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class EventSourceConfiguration {
--- producer/src/main/java/com/yury/wikimedia/producer/configuration/KafkaConfiguration.java
+++ producer/src/main/java/com/yury/wikimedia/producer/configuration/KafkaConfiguration.java
@@ -1,6 +1,7 @@
 package com.yury.wikimedia.producer.configuration;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +13,14 @@ public class KafkaConfiguration {
     @Bean
     public KafkaProducer<String, String> kafkaProducer() {
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "localhost:9092");
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, "" + 32 * 1024);
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
 
         return new KafkaProducer<>(properties);
     }
